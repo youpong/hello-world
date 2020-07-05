2619a3e3c4a3a79cb3ae1e56faeb2af30435503f
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-07-06 08:04:40 +0900

    add fib.h fib.c

---
+++ fib.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fib.h"
+
+int fibonacci(int n) {
+  if (n > 1) {
+    return fibonacci(n - 2) + fibonacci(n - 1);
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int main(int argc, char** argv) {
+  int n = 42;
+  
+  if (argc == 2) {
+    n = atoi(argv[1]);
+  }
+
+  printf("%d\n", fibonacci(n));
+
+  return 0;
+}
