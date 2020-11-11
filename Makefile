66078bba416721d3bf03734844e4f2c8266d0a99
youpong <43817652+youpong@users.noreply.github.com>
2020-11-11 22:37:23 +0900

    Dev2 (#5)
    * add Makefile
    
    * add unit test.

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
-.PHONY: all clean format
+.PHONY: all clean format check
 
 all: fib
+
 clean:
 	- rm *.o fib
+
 format:
 	clang-format -i *.[ch]
+
+check: all
+	./fib -test
+
 fib.o: fib.c fib.h
--- fib.c
+++ fib.c
@@ -1,6 +1,7 @@
 #include "fib.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int fibonacci(int n) {
   if (n > 1) {
@@ -12,8 +13,26 @@ int fibonacci(int n) {
   }
 }
 
+int my_exit(int ex) { exit(ex); }
+
+static void test_fibonacci() {
+  fibonacci(0) == 0 || my_exit(1);
+  fibonacci(1) == 1 || my_exit(1);
+  fibonacci(2) == 1 || my_exit(1);
+  fibonacci(3) == 2 || my_exit(1);
+  fibonacci(9) == 34 || my_exit(1);
+}
+
 int main(int argc, char **argv) {
   int n = 42;
+
+  if (argc == 2 && strcmp(argv[1], "-test") == 0) {
+    test_fibonacci();
+    printf("========================\n");
+    printf("All unit tests passed.\n");
+    printf("========================\n");
+    return 0;
+  }
 
   if (argc == 2) {
     n = atoi(argv[1]);
